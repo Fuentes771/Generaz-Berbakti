215fe0917c5321f0f311db14a884da1f1b824023
Fuentes <sulton843@gmail.com>
2025-07-09 18:40:57 +0700

    sync

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>PENDETEKSI DINI TSUNAMI RINOVA</title>
+<title>PENDETEKSI DINI TSUNMI RINOVA</title>
 
 <!-- CSS External -->
 <link rel="stylesheet" href="style/styles.css" />
