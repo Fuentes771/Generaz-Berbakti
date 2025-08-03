6ab517d95141f9ed7204ef44be513217522ac34d
Fuentes <sulton843@gmail.com>
2025-08-03 23:13:52 +0700

    j

--- includes/navbar.php
+++ includes/navbar.php
@@ -19,9 +19,8 @@ $navConfig = NAVBAR_CONFIG;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= BASE_URL ?>../assets/css/navbar.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
 </head>
